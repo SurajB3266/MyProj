1b28b1a3eec989e81fb418422e1f40a3c811b590
SurajBiradar <suraj.biradar@realthingks.com>
2023-02-23 20:16:54 +0530

    'CAN_Automation_Tool'

---
+++ setup_env_for_raft.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Installing Python modules...
+echo.
+
+pip install autosar
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install tkinter
+pip install pillow
+pip install customtkinter
+pip install openpyxl
+pip install jinja2
+pip install IPython
+pip install flask
+
+echo.
+echo Installation complete.
+pause
